f3d18506f8d898d0654b359fcff62192bc38c31a
Ki Dhakshana <dhakshanak@yahoo.com>
2013-05-07 19:57:17 -0700

    CS540Assing 8

---
+++ CS540Proj8/CS540Proj8/Form1.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.bExit = New System.Windows.Forms.Button()
+        Me.bNext = New System.Windows.Forms.Button()
+        Me.resizeFontLabel = New System.Windows.Forms.Label()
+        Me.resizeFontLabelAlt = New System.Windows.Forms.Label()
+        Me.statusBarLabel = New System.Windows.Forms.Label()
+        Me.resizeControlLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'bExit
+        '
+        Me.bExit.Location = New System.Drawing.Point(321, 10)
+        Me.bExit.Name = "bExit"
+        Me.bExit.Size = New System.Drawing.Size(75, 23)
+        Me.bExit.TabIndex = 0
+        Me.bExit.Text = "Exit"
+        Me.bExit.UseVisualStyleBackColor = True
+        '
+        'bNext
+        '
+        Me.bNext.Location = New System.Drawing.Point(16, 10)
+        Me.bNext.Name = "bNext"
+        Me.bNext.Size = New System.Drawing.Size(75, 23)
+        Me.bNext.TabIndex = 1
+        Me.bNext.Text = "Next"
+        Me.bNext.UseVisualStyleBackColor = True
+        '
+        'resizeFontLabel
+        '
+        Me.resizeFontLabel.BackColor = System.Drawing.Color.Cyan
+        Me.resizeFontLabel.Font = New System.Drawing.Font("Lucida Sans Unicode", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.resizeFontLabel.Location = New System.Drawing.Point(12, 96)
+        Me.resizeFontLabel.Name = "resizeFontLabel"
+        Me.resizeFontLabel.Size = New System.Drawing.Size(384, 185)
+        Me.resizeFontLabel.TabIndex = 2
+        Me.resizeFontLabel.Text = "font label"
+        '
+        'resizeFontLabelAlt
+        '
+        Me.resizeFontLabelAlt.AutoSize = True
+        Me.resizeFontLabelAlt.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.resizeFontLabelAlt.Font = New System.Drawing.Font("Lucida Sans Unicode", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.resizeFontLabelAlt.Location = New System.Drawing.Point(34, 53)
+        Me.resizeFontLabelAlt.Name = "resizeFontLabelAlt"
+        Me.resizeFontLabelAlt.Size = New System.Drawing.Size(70, 15)
+        Me.resizeFontLabelAlt.TabIndex = 3
+        Me.resizeFontLabelAlt.Text = "fontlabel alt"
+        '
+        'statusBarLabel
+        '
+        Me.statusBarLabel.AutoSize = True
+        Me.statusBarLabel.BackColor = System.Drawing.Color.DimGray
+        Me.statusBarLabel.Font = New System.Drawing.Font("Lucida Sans Unicode", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.statusBarLabel.Location = New System.Drawing.Point(159, 371)
+        Me.statusBarLabel.Name = "statusBarLabel"
+        Me.statusBarLabel.Size = New System.Drawing.Size(61, 15)
+        Me.statusBarLabel.TabIndex = 4
+        Me.statusBarLabel.Text = "status bar"
+        '
+        'resizeControlLabel
+        '
+        Me.resizeControlLabel.AutoSize = True
+        Me.resizeControlLabel.BackColor = System.Drawing.Color.MediumSpringGreen
+        Me.resizeControlLabel.Font = New System.Drawing.Font("Lucida Sans Unicode", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.resizeControlLabel.Location = New System.Drawing.Point(218, 53)
+        Me.resizeControlLabel.Name = "resizeControlLabel"
+        Me.resizeControlLabel.Size = New System.Drawing.Size(77, 23)
+        Me.resizeControlLabel.TabIndex = 5
+        Me.resizeControlLabel.Text = "control"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(404, 395)
+        Me.Controls.Add(Me.resizeControlLabel)
+        Me.Controls.Add(Me.statusBarLabel)
+        Me.Controls.Add(Me.resizeFontLabelAlt)
+        Me.Controls.Add(Me.resizeFontLabel)
+        Me.Controls.Add(Me.bNext)
+        Me.Controls.Add(Me.bExit)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents bExit As System.Windows.Forms.Button
+    Friend WithEvents bNext As System.Windows.Forms.Button
+    Friend WithEvents resizeFontLabel As System.Windows.Forms.Label
+    Friend WithEvents resizeFontLabelAlt As System.Windows.Forms.Label
+    Friend WithEvents statusBarLabel As System.Windows.Forms.Label
+    Friend WithEvents resizeControlLabel As System.Windows.Forms.Label
+
+End Class
---
+++ CS540Proj8/CS540Proj8/Form1.vb
@@ -0,0 +1,108 @@
+﻿Imports System
+Imports System.IO
+
+Public Class Form1
+    Inherits System.Windows.Forms.Form
+
+    Dim bWeHaveAValidInputFile As Boolean = False
+    Dim stringInputStringFile As FileStream
+    Dim stringReader As StreamReader
+    Dim workingString As String = ""
+    Const FILE_PATH As String = "C:\Users\Ki\Documents\CS540Proj8\CS540Proj8\cs540hw8input.txt"
+
+    Private Function readValidInputTextfile(ByVal fileName As String)
+        Try
+            stringInputStringFile = New FileStream(fileName, FileMode.Open, FileAccess.Read)
+            stringReader = New StreamReader(stringInputStringFile)
+            bWeHaveAValidInputFile = True
+            Return True
+        Catch ex As Exception
+            bWeHaveAValidInputFile = False
+            Return False
+        End Try
+    End Function
+
+    Private Function getNextString(ByRef reader As StreamReader)
+        Dim readString As String
+        If reader.Peek() > -1 Then
+            readString = reader.ReadLine()
+        Else
+            readString = ""
+        End If
+        Return readString
+    End Function
+
+
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        If readValidInputTextfile(FILE_PATH) Then
+            statusBarLabel.Text = ""
+            workingString = getNextString(stringReader)
+            processString(workingString)
+        Else
+            statusBarLabel.Text = "Input text file not found or is invalid"
+        End If
+    End Sub
+
+    Private Sub processString(ByVal str As String)
+        Dim f1 As Font
+        Dim g1 As Graphics
+        Dim s1 As SizeF
+        Dim marginBuffer As Integer
+        g1 = Me.resizeFontLabel.CreateGraphics
+        s1 = g1.MeasureString(str, resizeControlLabel.Font)
+        marginBuffer = g1.MeasureString("  ", resizeControlLabel.Font).Width
+        resizeControlLabel.Width = s1.Width + marginBuffer
+        g1.Dispose()
+
+        resizeControlLabel.Text = str
+        Dim f2 As Font
+        Dim g2 As Graphics
+        Dim s2 As SizeF
+        Dim Faktor, FaktorX, FaktorY As Single
+        g2 = Me.resizeFontLabel.CreateGraphics
+        s2 = g2.MeasureString(" " + str, resizeFontLabel.Font)
+        g2.Dispose()
+        FaktorX = resizeFontLabel.Width / s2.Width
+        FaktorY = resizeFontLabel.Height / s2.Height
+        If FaktorX > FaktorY Then
+            Faktor = FaktorX
+        Else
+            Faktor = FaktorX
+        End If
+        f2 = resizeFontLabel.Font
+        resizeFontLabel.Font = New Font(f2.Name, f2.SizeInPoints * Faktor)
+        resizeFontLabel.Height = resizeFontLabel.Font.Height
+        resizeFontLabel.Text = str
+
+        Dim f3 As Font
+        Dim g3 As Graphics
+        Dim s3 As SizeF
+        Dim faktor1, faktor1x, faktor1y As Single
+        g3 = Me.resizeFontLabelAlt.CreateGraphics
+        s3 = g3.MeasureString("   " + str, resizeFontLabelAlt.Font)
+        g3.Dispose()
+        faktor1 = resizeFontLabelAlt.Width / s3.Width
+        f3 = resizeFontLabelAlt.Font
+        resizeFontLabelAlt.Font = New Font(f3.Name, f3.SizeInPoints * faktor1)
+
+        resizeFontLabelAlt.Height = resizeFontLabelAlt.Font.Height
+
+        resizeFontLabelAlt.Text = str
+    End Sub
+
+    Private Sub bNext_Click(sender As System.Object, e As System.EventArgs) Handles bNext.Click
+        If bWeHaveAValidInputFile Then
+            workingString = getNextString(stringReader)
+            If workingString <> "" Then
+                processString(workingString)
+            Else
+                End
+            End If
+        End If
+    End Sub
+
+    Private Sub bExit_Click(sender As System.Object, e As System.EventArgs) Handles bExit.Click
+        End
+    End Sub
+End Class
